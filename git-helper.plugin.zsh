4e55bfe8dd0a9e0032033e7f6447ebf86052167e
Edison Guo <hydra1983@gmail.com>
2014-08-30 14:16:36 +0100

    update

--- git-helper.plugin.zsh
+++ git-helper.plugin.zsh
@@ -19,7 +19,7 @@ function _git_helper_push_2_all {
 	git push origin --all
 }
 
-function _git_helper_replace_user_email_push_2_origin_master {
+function _git_helper_replace_name_email_push_2_origin_master {
 	user_name=$1
 	user_email=$2
 
@@ -39,6 +39,6 @@ function ghacp2om {
 	_git_helper_push_2_origin_master
 }
 
-function ghrnep@om {
-	_git_helper_replace_name_email_push_2_origin_master
+function ghrnep2om {
+	_git_helper_replace_name_email_push_2_origin_master $1 $2
 }
