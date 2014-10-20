39b203448fcebc39304f8963b7b8927d4b82c431
Edison Guo <hydra1983@gmail.com>
2014-10-20 22:45:16 +0800

    Fix

--- git-helper.plugin.zsh
+++ git-helper.plugin.zsh
@@ -1,8 +1,8 @@
 if [ -L $0 ] ; then
-    _gh_helper_base=$(dirname $(readlink -f $0)) ;
+    _gh_helper_base=$(dirname $(readlink -f $0))
 else
-    _gh_helper_base=$(dirname $0) ;
-fi ;
+    _gh_helper_base=$(dirname $0)
+fi
 
 function _git_helper_add_all {
 	git add -A .
@@ -12,7 +12,7 @@ function _git_helper_commit {
 	if [[ "$1" == "" ]] ; then
 		message="update"
 	else
-		message=$1
+		message="$1"
 	fi
 	git commit -m "$message"
 }
@@ -26,43 +26,43 @@ function _git_helper_push_2_all {
 }
 
 function _git_helper_replace_name_email_push_2_origin_master {
-	user_name=$1
-	user_email=$2
+	user_name="$1"
+	user_email="$2"
 
-	git filter-branch -f --env-filter "GIT_AUTHOR_NAME='$user_name'; GIT_AUTHOR_EMAIL='$user_email'; GIT_COMMITTER_NAME='$user_name'; GIT_COMMITTER_EMAIL='$user_email';" HEAD
+	git filter-branch -f --env-filter "GIT_AUTHOR_NAME='${user_name}'; GIT_AUTHOR_EMAIL='${user_email}'; GIT_COMMITTER_NAME='${user_name}'; GIT_COMMITTER_EMAIL='${user_email}';" HEAD
 	git push -f origin master
 }
 
 function _git_remote_add {
-	remote=$1
-	remote_url=$2
+	remote="$1"
+	remote_url="$2"
 
-	if [[ "$remote" != "" ]] && [[ "$remote_url" != "" ]] ; then
-		[[ "$(git remote | grep $remote)" == "" ]] || git remote remove $remote
+	if [[ "${remote}" != "" ]] && [[ "${remote_url}" != "" ]] ; then
+		[[ "$(git remote | grep ${remote})" == "" ]] || git remote remove "${remote}"
 
-		echo "Add remote : [$remote] $remote_url"
-		git remote add $remote $remote_url
+		echo "Add remote : [${remote}] ${remote_url}"
+		git remote add "${remote}" "${remote_url}"
 	fi	
 }
 
 function ghacp2all {
 	_git_helper_add_all
-	_git_helper_commit $1
+	_git_helper_commit "$1"
 	_git_helper_push_2_all
 }
 
 function ghacp2om {
 	_git_helper_add_all
-	_git_helper_commit $1
+	_git_helper_commit "$1"
 	_git_helper_push_2_origin_master
 }
 
 function ghrnep2om {
-	_git_helper_replace_name_email_push_2_origin_master $1 $2
+	_git_helper_replace_name_email_push_2_origin_master "$1" "$2"
 }
 
 function ghrao {
-	_git_remote_add origin $1
+	_git_remote_add origin "$1"
 }
 
 function _ghacp2om_usage {
@@ -70,26 +70,26 @@ function _ghacp2om_usage {
 }
 
 function _ghacp2om_exec() {
-	dir=$1
-	message=$2
+	dir="$1"
+	message="$2"
 	if [[ "$dir" = "" ]] ; then
-		pushd $_gh_helper_base 1> /dev/null
-			ghacp2om $message
+		pushd "${_gh_helper_base}" 1> /dev/null
+			ghacp2om "${message}"
 		popd 1> /dev/null
 	else
-		isPath=$(echo "$dir"|grep ^\/)
-		if [[ "$isPath" == "" ]] ; then
-			pluginName=$dir
-			pluginDir=$_gh_helper_base/../$pluginName
-			if [ -d $pluginDir -a -d $pluginDir/.git ] ; then
-				pushd $pluginDir 1> /dev/null
-				ghacp2om $message
+		isPath=$(echo "${dir}"|grep ^\/)
+		if [[ "${isPath}" == "" ]] ; then
+			pluginName="${dir}"
+			pluginDir="${_gh_helper_base}/../${pluginName}"
+			if [ -d "${pluginDir}" -a -d "${pluginDir}/.git" ] ; then
+				pushd "${pluginDir}" 1> /dev/null
+				ghacp2om "${message}"
 				popd 1> /dev/null
 			fi
 		else
-			if [ -d $dir -a -d $dir/.git ] ; then
-				pushd $dir 1> /dev/null
-				ghacp2om $message
+			if [ -d "${dir}" -a -d "${dir}/.git" ] ; then
+				pushd "${dir}" 1> /dev/null
+				ghacp2om "${message}"
 				popd 1> /dev/null
 			fi
 		fi
@@ -98,22 +98,22 @@ function _ghacp2om_exec() {
 
 function _ghacp2om {		
 	while getopts ":m:" opt; do
-	  case $opt in
+	  case "${opt}" in
 	    m)
-	      	message=$OPTARG
+	      	message="${OPTARG}"
 	      	;;
 	    \?)
-	      	echo "Invalid option: -$OPTARG" >&2
+	      	echo "Invalid option: -${OPTARG}" >&2
 	      	;;
 	    :)
-	      	echo "Option -$OPTARG requires an argument." >&2
+	      	echo "Option -${OPTARG} requires an argument." >&2
 	      	;;
 	    *)
-			echo $OPTARG
+			echo "${OPTARG}"
 			;;
 	  esac
 	done
 
-	dir=${@:$OPTIND:1}
-	_ghacp2om_exec "$dir" "$message"
+	dir="${@:${OPTIND}:1}"
+	_ghacp2om_exec "${dir}" "${message}"
 }
